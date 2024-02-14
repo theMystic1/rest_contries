eec5a1099edfe859a0a5f6e129a97e68ef5abdf2
theMystic <cluckyugo@gmail.com>
2024-02-14 08:32:26 +0100

    lfg🔥🔥🔥🔥

--- src/Filter.js
+++ src/Filter.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Countries } from "./Countries";
 
 export const region = [
   {
--- src/error.js
+++ src/error.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 export function ErrorMessage({ error }) {
   // const [error, setError] = useState();
   return <p className="error">{error}</p>;
